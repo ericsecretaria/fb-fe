67bdaf4a0f70e5e75173f14f57c9a69b72a65a4e
esecretaria <ejsecretaria@gmail.com>
2024-05-12 04:00:38 -0600

    base url updated with live url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+// const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://foodbook-be.onrender.com/api/v1";
 
 export default BASE_URL;
